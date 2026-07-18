b76bcea13eb4525ce1ccec17bcee9c095246f00e
Spider Net <ranndaraitofficial@gmail.com>
2026-07-18 21:27:13 +0700

    Add files via upload

---
+++ Bash_Scripting/Reset_Navicat/navicat_reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Resetting Navicat Premium Trial..."
+dconf reset -f /com/premiumsoft/navicat-premium/
+if [ -f ~/.config/navicat/Premium/preferences.json ]; then
+    sed -i -E 's/,?"([ A-F0-9]+)":\{([^\}]+)},?//g' ~/.config/navicat/Premium/preferences.json
+fi
+echo "Done. Restart Navicat."
