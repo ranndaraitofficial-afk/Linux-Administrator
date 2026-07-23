35e24bc723ba6b4323020649ebeaf41a738ea8c1
Spider Net <ranndaraitofficial@gmail.com>
2026-07-23 08:35:14 +0700

    Update navicat_reset.sh

--- Bash_Scripting/Reset_Navicat/navicat_reset.sh
+++ Bash_Scripting/Reset_Navicat/navicat_reset.sh
@@ -1,7 +1,7 @@
 #Note : im not sure who is the owner of this script i just find it accidenly by google search engine 
 #so i decide to test it and find it work ;
 #try it with responsibility
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Resetting Navicat Premium Trial..."
 dconf reset -f /com/premiumsoft/navicat-premium/
 if [ -f ~/.config/navicat/Premium/preferences.json ]; then
